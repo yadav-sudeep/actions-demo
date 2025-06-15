14d7ad341101cb9da065505f41d90aa72133b6b7
yadav-sudeep <166811024+yadav-sudeep@users.noreply.github.com>
2025-06-15 14:47:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
-# pylint: skip-file
-"print('Running Lint')" 
-"print('Here is an example of line too long ............................................................................................................................................... this might show up in the lint score ....................................................................... and it should show an error')" 
+print('Running Lint')
+print('Here is an example of line too long ............................................................................................................................................... this might show up in the lint score ....................................................................... and it should show an error')
+
+a = True
+
+if a is True:
+  print("1")
+  if a is True:
+    print("1")
+    if a is True:
+      print("1")
+      if a is True:
+        print("1")
+      else:
+        print("2")
+    else:
+      print("2")
+  else:
+    print("2")
+else:
+  print("2")
