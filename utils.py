19e3fd98d17920b415247dcd72dfacbea8cc7337
yadav-sudeep <166811024+yadav-sudeep@users.noreply.github.com>
2025-06-15 14:38:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,3 @@
+# pylint: skip-file
 "print('Running Lint')" 
 "print('Here is an example of line too long ............................................................................................................................................... this might show up in the lint score ....................................................................... and it should show an error')" 
